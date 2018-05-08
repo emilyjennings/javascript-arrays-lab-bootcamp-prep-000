25d5ae645e6fb1d1c7fc28bfd720d6ccc269683f
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-08 00:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
-destructivelyAppendKitten("Sasha") {
-  kittens.push("Sasha")
+destructivelyAppendKitten(name) {
+  name = "Sasha"
+  kittens.push(name)
 }
