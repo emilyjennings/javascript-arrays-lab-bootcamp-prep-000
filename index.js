f3641495e1ef353a7b196f0b4f79cea8554da25b
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-08 00:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(){
 }
 
 function removeLastKitten(){
-  var newArray = kittens.slice(1, kittens.length-2);
+  var newArray = kittens.slice(0, kittens.length-1);
   return newArray;
 }
 
